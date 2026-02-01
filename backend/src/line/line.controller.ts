680bd063f0c261ee929b71de35ab123a76c2e800
mori <hokuto.8160@gmail.com>
2026-02-01 15:23:11 +0900

    20260201

--- backend/src/line/line.controller.ts
+++ backend/src/line/line.controller.ts
@@ -1,22 +1,20 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { LineService } from './line.service';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 
 @Controller('line')
-@UseGuards(JwtAuthGuard)
 export class LineController {
   constructor(private readonly lineService: LineService) {}
 
   @Post('webhook')
-  async webhook(@Req() req: any, @CurrentUser() user: any) {
+  async webhook(@Req() req: any) {
     const events = req.body.events;
 
     console.log(events);
-    console.log('user', user);
+    // console.log('user', user);
 
     for (const event of events) {
-      await this.lineService.handleEvent(event, user.lineLoginId);
+      await this.lineService.handleEvent(event, 0);
     }
 
     return 'ok';
