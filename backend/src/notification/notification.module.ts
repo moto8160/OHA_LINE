b4ed456ceecf20a072b4efff64c776af5e44dce5
mori <hokuto.8160@gmail.com>
2026-02-02 22:27:55 +0900

    20260202

--- backend/src/notification/notification.module.ts
+++ backend/src/notification/notification.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
+import { NotificationScheduler } from './notification.scheduler';
 import { LineModule } from '../line/line.module';
 import { TodoModule } from '../todo/todo.module';
 import { PrismaService } from '../prisma.service';
@@ -8,6 +9,6 @@ import { PrismaService } from '../prisma.service';
 @Module({
   imports: [LineModule, TodoModule],
   controllers: [NotificationController],
-  providers: [NotificationService, PrismaService],
+  providers: [NotificationService, NotificationScheduler, PrismaService],
 })
 export class NotificationModule {}
